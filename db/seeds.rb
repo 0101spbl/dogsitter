b0dde25c62fe715096b09f18b0d9ce19d69709b7
0101spbl <spbl@outlook.fr>
2022-10-28 00:43:00 +0200

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,67 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+nb_dogs = 75
+nb_dogsitters = 150
+nb_strolls = 50
+nb_cities = 7
+
+############################################
+# CITIES
+
+nb_cities.times do |i|
+  City.create!(
+    city_name: Faker::Address.city,
+  )
+end
+puts "#{nb_cities} cities built!"
+
+############################################
+# DOGS
+
+nb_dogs.times do |i|
+  Dog.create!(
+    name: Faker::Creature::Dog.name,
+    breed: Faker::Creature::Dog.breed,
+    gender: Faker::Creature::Dog.gender,
+    city_id: City.all.shuffle.last.id,
+    is_a_good_dog: true,
+  )
+end
+puts "#{nb_dogs} dogs created!"
+
+############################################
+# DOGSITTERS
+
+nb_dogsitters.times do |i|
+  Dogsitter.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    gender: Faker::Gender.type,
+    age: rand(18..70),
+    city_id: City.all.shuffle.last.id,
+  )
+end
+puts "#{nb_dogsitters} dogsitters created!"
+
+
+############################################
+# STROLLS
+
+nb_strolls.times do |i|
+  Stroll.create!(
+    date: Faker::Time.between_dates(from: Date.today, to: Date.today + 30, period: :day),
+    dog_id: Dog.all.shuffle.last.id,
+    dogsitter_id: Dogsitter.all.shuffle.last.id,
+    )
+end
+
+puts "#{nb_strolls} strolls scheduled!"
+
+############################################
+
+puts "Seeding done!"
